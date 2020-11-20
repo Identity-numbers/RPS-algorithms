2b4c073f7cec70a56b531e145fde773706f57b28
Linus Jarbo <linus@linusjarbo.com>
2020-11-20 14:40:58 +0100

    changing branch

--- RPS-algorithm/Assets/Scripts/Main.cs
+++ RPS-algorithm/Assets/Scripts/Main.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    //add three objects
+    public GameObject SphereA;
+    public GameObject SphereB;
+    public GameObject SphereC;
+
+    private Vector3 vectorA;
+    private Vector3 vectorB;
+    private Vector3 vectorC; 
+
+    public List<GameObject> gameObjList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObjList.Add(SphereA);
+        gameObjList.Add(SphereB);
+        gameObjList.Add(SphereC);
+
+        vectorA = SphereA.transform.position;
+        vectorB = SphereB.transform.position;
+        vectorC = SphereC.transform.position; 
+    }
+
+    public void ResetAll()
+    {
+        SphereA.transform.position = vectorA;
+        SphereB.transform.position = vectorB;
+        SphereC.transform.position = vectorC;
+
+        for (int i = 0; i < gameObjList.Count; i++)
+        {
+            gameObjList[i].GetComponent<SphereProperties>().ResetVelocity();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //update velocity
+        for (int i = 0; i < gameObjList.Count; i++)
+        {
+            gameObjList[i].GetComponent<SphereProperties>().UpdateVelocity();
+        }
+
+        //update position
+                for (int i = 0; i < gameObjList.Count; i++)
+        {
+            gameObjList[i].GetComponent<SphereProperties>().UpdatePostion();
+        }
     }
 }
---
+++ RPS-algorithm/Assets/Scripts/SphereProperties.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SphereProperties : MonoBehaviour
+{
+    public GameObject FollowingObj;
+    public Slider sliderStepMag;
+    private float velocity_x = 0f;
+    private float velocity_y = 0f;
+    private float stpMag = 1000f;
+
+    float other_x;
+    float other_y;
+    float this_x;
+    float this_y;
+
+    private void Update()
+    {
+        stpMag = sliderStepMag.value;
+    }
+
+    public void ResetVelocity()
+    {
+        velocity_x = 0f;
+        velocity_y = 0f;
+    }
+
+    // updates called by main
+    public void UpdateVelocity()
+    {
+        //get x and y values of other object
+        other_x = 1f * FollowingObj.transform.position.x;
+        other_y = 1f * FollowingObj.transform.position.y;
+        this_x = 1f * gameObject.transform.position.x;
+        this_y = 1f * gameObject.transform.position.y;
+
+        //set velocity
+        velocity_x += -(this_x - other_x) / (stpMag);
+        velocity_y += -(this_y - other_y) / (stpMag);
+    }
+    public void UpdatePostion()
+    {
+        Vector3 temp = new Vector3(velocity_x, velocity_y, 0);
+        gameObject.transform.position += temp;
+    }
+}
